79282cf0ff2ec93ab2d8a609dff53da255bb3530
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-02-12 03:49:26 +0000

    [Add] 投稿機能の部分テンプレート追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,17 @@
+class BooksController < ApplicationController
+  def create
+  end
+
+  def index
+    @book = Book.new
+  end
+
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+end
