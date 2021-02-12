ef1341517f411d0aaedbf7d8135db2f6a0eaf10e
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-02-12 08:20:43 +0000

    [Add] ユーザー別Book一覧の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: 'homes#top'  
   devise_for :users
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
