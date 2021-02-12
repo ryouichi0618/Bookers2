1ee05aa903c59b361dc6ebe7f88aebedc3a32c17
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-02-12 07:43:47 +0000

    [Add] 投稿削除機能、投稿編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'  
   devise_for :users
-  resources :books, only: [:create, :index, :show, :edid, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
 end
