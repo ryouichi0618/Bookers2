14c1be9bc1c30db69b5a482be60b0d8879c6c3e8
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-02-12 04:11:35 +0000

    [Add] 投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,16 @@
 class BooksController < ApplicationController
-  def create
-  end
+  
 
   def index
     @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -13,5 +20,10 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
