65193d74b57803657d9c4a6fa5eadb34a6cf7e74
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-02 17:15:06 +0000

    [Add]いいね機能の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,11 @@ class Book < ApplicationRecord
   validates :body, presence: true, length: { maximum: 200 }
 
   has_many :book_comments
+  has_many :favorites
   belongs_to :user
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  
 end
